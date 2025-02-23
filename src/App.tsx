f7b2460945af374e6e335c251f486392a90bcf99
AjiNurAji <ajinuraji090306@gmail.com>
2025-02-23 10:15:34 +0700

    :package: install shadcn component ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "~/components/ui/button";
+
 const App = () => {
   return (
     <div className="min-h-screen h-full w-full flex flex-col justify-center items-center">
       WELCOME TO REACT TSX + TAILWIND CSS + VITE
+      <Button variant="link">Shadcn</Button>
     </div>
   );
 };
